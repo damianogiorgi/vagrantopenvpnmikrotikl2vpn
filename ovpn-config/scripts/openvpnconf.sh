76928327aae59330db27f86f3c948c980b85dc43
Damiano Giorgi <dgiorgi@facilitylive.com>
2019-08-27 15:38:36 +0200

    working config with bridge, missing auto mikrotik config

--- ovpn-config/scripts/openvpnconf.sh
+++ ovpn-config/scripts/openvpnconf.sh
@@ -4,6 +4,9 @@
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 sysctl -p
 cp -f /vagrant/ovpn-config/$1/openvpn.conf /etc/openvpn/
+if [ -d /vagrant/ovpn-config/$1/ccd ]; then
+    cp -a /vagrant/ovpn-config/$1/ccd /etc/openvpn/
+fi
 cp -f /vagrant/ovpn-config/scripts/openvpn@.service  /etc/systemd/system
 systemctl daemon-reload
 systemctl enable openvpn@$1
